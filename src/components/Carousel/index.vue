c3afcd42a09eb4bc5d44d0a41c7a89d46206f1b1
jiale <810152974@qq.com>
2022-04-24 15:12:46 +0800

    update

---
+++ src/components/Carousel/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="swiper-container" ref="cur">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="carousel in list" :key="carousel.id">
+        <img :src="carousel.imgUrl" />
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+export default {
+  name: "Carousel", 
+  props: ["list"],
+  watch: {
+    list: {
+      immediate: true,
+      handler() {
+        this.$nextTick(() => {
+          var mySwiper = new Swiper(this.$refs.cur, {
+            loop: true, // 循环模式选项
+            // 如果需要分页器
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            autoplay: {
+          delay: 3000,
+          stopOnLastSlide: false,
+          disableOnInteraction: true,
+        },
+
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+
+            // 如果需要滚动条
+            scrollbar: {
+              el: ".swiper-scrollbar",
+            },
+          });
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
